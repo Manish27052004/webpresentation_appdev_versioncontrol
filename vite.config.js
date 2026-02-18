988157e7b46a35513640a8d1c36f017e9f824f12
Manish27052004 <175503346+Manish27052004@users.noreply.github.com>
2026-02-18 19:27:26 +0530

    deployment error resolved

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
                 main: resolve(__dirname, 'index.html'),
                 module1: resolve(__dirname, 'module1.html'),
                 module2: resolve(__dirname, 'module2.html'),
+                module3: resolve(__dirname, 'module3.html'),
             }
         }
     }
